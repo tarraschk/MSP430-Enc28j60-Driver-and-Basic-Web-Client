d1d5db03936a249bdd1c614755b24fe4bafb3986
Maxime Alay-Eddine <maxime.alayeddine@gmail.com>
2013-08-13 17:06:41 +0200

    Update spi.h

--- spi.h
+++ spi.h
@@ -1,9 +1,11 @@
 #ifndef SPI_H
 #define SPI_H
 
-// public functions
+// Function Prototypes
 void initSPI(void);
 unsigned int SPIWrite(unsigned char*, unsigned int);
 unsigned int SPIRead(unsigned char*, unsigned int);
+void enc_lightLeds(void);
+void enc_shutdownLeds(void);
 
-#endif
+#endif /* SPI_H */
